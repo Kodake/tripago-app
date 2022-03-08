3b327692c69d6089934ff7e07f4dae1ae1558e72
Kodake <kodake@hotmail.es>
2022-03-08 01:10:28 -0400

    Adding styles to buttons and dropdown

--- src/components/TripList.tsx
+++ src/components/TripList.tsx
@@ -10,16 +10,32 @@ interface Trip {
 const TripList = () => {
 
     const [trips, setTrips] = useState([]);
-
+    const [url, setUrl] = useState('http://localhost:3000/trips');
+    
     useEffect(() => {
-        fetch('http://localhost:3000/trips')
+        fetch(url)
             .then(data => data.json())
             .then(json => setTrips(json));
-    }, [])
+    }, [url]);
 
     return (
         <div className='trip-list'>
             <h2>TripList</h2>
+
+            <label>
+                <span>Filter:</span>
+                <select className='select' onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setUrl(e.target.value)}>
+                    <option value={'http://localhost:3000/trips'}>All Trips</option>
+                    <option value={'http://localhost:3000/trips?loc=Europe'}>European Trips</option>
+                </select>
+            </label>
+
+            <div className='filters'>
+                <button onClick={() => setUrl('http://localhost:3000/trips')}>All Trips</button>
+
+                <button onClick={() => setUrl('http://localhost:3000/trips?loc=Europe')} >European Trips</button>
+            </div>
+
             <ul>
                 {trips.map((trip: Trip) => {
                     return (
@@ -27,7 +43,6 @@ const TripList = () => {
                             <h3>{trip.title}</h3>
                             <p>{trip.price}</p>
                         </li>
-
                     )
                 })}
             </ul>
